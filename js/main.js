60e1fd358374e1669a99490aeed5867f6581579c
Justin Brunner <brunner.justin.jakob@gmail.com>
2017-01-26 10:37:39 -0700

    Changes to js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ function init() {
         $('.main').fadeOut();
         $('#setup').fadeIn();
 
-        $('#setup').keypress(function(event) {
+        $('#name-input').focus();
+        $('#name-input').keypress(function(event) {
             var keycode = (event.keyCode ? event.keyCode : event.which);
             if(keycode == '13'){
                 var nameInput = $('#name-input').val();
@@ -19,13 +20,36 @@ function init() {
     });
 
     function generateGame() {
-        var scenario = Math.floor((Math.random() * 5) + 1);
+        var scenario =  Math.floor((Math.random() * 4) + 1);
 
         $('#scenario_' + scenario).fadeIn();
+        //scenarioOne();
     }
 
     /*
     * Provide Scenario specific functions below within inti();
     */
+    function scenarioOne() {
+        $(body).keypress(function(e) {
+            var horiz_pos = $('#dragon_slayer_1').css('left');
+            var vert_pos = $('#dragon_slayer_1').css('top');
+            console.log(horiz_pos);
+            left = 37
+            up = 38
+            right = 39
+            down = 40
+            if (e.keyCode == 37) {
+                $('#drgn_slyr_1').animate({ left: horiz_pos + 5 });
+            } else if (e.keyCode == 38) {
 
+            } else if (e.keyCode == 39) {
+
+            } else if (e.keyCode == 40) {
+
+            }
+
+
+
+        });
+    }
 }
